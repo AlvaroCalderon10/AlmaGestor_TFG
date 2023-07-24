b7c2339983ae1bec75bf85e1273a3fc3e95c1093
Álvaro Calderón <alvaro_30601@hotmail.es>
2023-07-24 21:00:59 +0200

    Delete and DTO fix on productBean

--- app/src/main/java/com/example/almagestor/ListAdapters/ListAdapterProductBean.java
+++ app/src/main/java/com/example/almagestor/ListAdapters/ListAdapterProductBean.java
@@ -8,11 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.almagestor.Products.ProductDataDTO;
 import com.example.almagestor.R;
+import com.example.almagestor.Sqlite.SqliteModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
@@ -42,6 +45,21 @@ public class ListAdapterProductBean extends RecyclerView.Adapter<ListAdapterProd
     @Override
     public void onBindViewHolder(final ListAdapterProductBean.ViewHolder holder, final int position){
         holder.bindData(mData.get(position));
+
+        holder.Delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String ean=holder.textEan.getText().toString();
+                ean=ean.replace("EAN: ","");
+                mData.remove(holder.getAdapterPosition());
+                notifyItemRemoved(holder.getAdapterPosition());
+                SqliteModel obj=new SqliteModel();
+                if(obj.delete_Product(context,ean)){
+                    Toast.makeText(context, context.getResources().getString(R.string.ProductoBorrado), Toast.LENGTH_SHORT).show();
+                };
+            }
+        });
     }
 
     public void setItems(List<ProductDataDTO> items){mData=items;}
@@ -49,6 +67,8 @@ public class ListAdapterProductBean extends RecyclerView.Adapter<ListAdapterProd
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView iconImage;
         TextView textProduct,textEan,textQuantite, textPrice;
+        FloatingActionButton Delete;
+
         ViewHolder(View itemView){
             super(itemView);
             iconImage=itemView.findViewById(R.id.IconImageView_productbean);
@@ -56,15 +76,16 @@ public class ListAdapterProductBean extends RecyclerView.Adapter<ListAdapterProd
             textEan=itemView.findViewById(R.id.product_ean_productbean);
             textQuantite=itemView.findViewById(R.id.quantite_productbean);
             textPrice=itemView.findViewById(R.id.product_price_productbean);
+            Delete=itemView.findViewById(R.id.btn_delete_productbean);
         }
         void bindData(final ProductDataDTO item){
             if(!item.getImg().equals("1")){
                 iconImage.setImageBitmap(BitmapFactory.decodeByteArray((Base64.getDecoder().decode(item.getImg())),0,(Base64.getDecoder().decode(item.getImg()).length)));
             }
             textProduct.setText(item.getNameProduct());
-            textEan.setText(item.getEan());
-            textQuantite.setText("1");
-            textPrice.setText("5");
+            textEan.setText("EAN: "+item.getEan());
+            textQuantite.setText(String.valueOf(item.getUnits()));
+            textPrice.setText(context.getResources().getString(R.string.info_product_price)+String.valueOf(item.getPrice()));
         }
     }
 }
